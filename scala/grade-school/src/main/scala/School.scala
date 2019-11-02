8acb5475acaf9563166b5bd3a60493537871695c
Mohamed Ali <mohamed.ali@alumni.ubc.ca>
2019-11-02 09:38:58 -0700

    simplified solution to grade-school problem

--- scala/grade-school/src/main/scala/School.scala
+++ scala/grade-school/src/main/scala/School.scala
@@ -1,21 +1,16 @@
 class School {
   type DB = Map[Int, Seq[String]]
 
-  private var dbLatestState: DB = Map[Int, Seq[String]]()
+  var db: DB = Map[Int, Seq[String]]()
 
-  def db() = dbLatestState
-
-  def add(name: String, g: Int): Unit = {
-    if (dbLatestState.contains(g)) dbLatestState = dbLatestState.updated(g, dbLatestState(g) :+ name)
-    else dbLatestState = dbLatestState + (g -> Seq(name))
-  }
+  def add(name: String, g: Int) = { db = db + (g -> (grade(g) :+ name))}
 
   def grade(g: Int): Seq[String] = {
-    dbLatestState.getOrElse(g, Seq())
+    db.getOrElse(g, Seq())
   }
 
   def sorted: DB = {
-    dbLatestState.mapValues(x => x.sorted).toSeq.sortBy(_._1).toMap
+    db.mapValues(x => x.sorted).toSeq.sortBy(_._1).toMap
   }
 
 }
